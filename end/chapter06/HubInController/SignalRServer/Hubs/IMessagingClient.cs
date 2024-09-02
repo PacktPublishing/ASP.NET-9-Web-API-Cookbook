3839c97190e9c3af821d3c31c194f8bd23ba7354
lavedon <lavedon@gmail.com>
2024-09-02 16:35:32 -0400

    HubInController end added

---
+++ end/chapter06/HubInController/SignalRServer/Hubs/IMessagingClient.cs
@@ -0,0 +1,7 @@
+public interface IMessagingClient
+{
+    Task ReceiveMessage(string user, string message);
+    Task UserConnected(string username);
+    Task UserDisconnected(string username);
+    Task UserLoggedIn(string username);
+}
---
+++ end/chapter06/HubInController/SignalRServer/Models/LoginDTO.cs
@@ -0,0 +1,7 @@
+namespace SignalRServer.LoginModel;
+
+public class LoginDTO
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
