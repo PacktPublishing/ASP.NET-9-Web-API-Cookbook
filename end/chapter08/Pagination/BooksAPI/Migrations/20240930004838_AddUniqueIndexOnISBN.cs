04e7a0f740c57a7e60db10e97ad16f94563ecd70
lavedon <lavedon@gmail.com>
2024-09-30 01:11:46 -0400

    pagination added

---
+++ end/chapter08/Pagination/BooksAPI/Migrations/20240930004838_AddUniqueIndexOnISBN.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace chapter5.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexOnISBN : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_ISBN",
+                table: "Books",
+                column: "ISBN",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Books_ISBN",
+                table: "Books");
+        }
+    }
+}
