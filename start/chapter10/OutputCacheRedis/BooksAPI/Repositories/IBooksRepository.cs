07bb1ffa5464b678b292abcee08f7c5dc6599a51
lavedon <lavedon@gmail.com>
2024-11-10 22:50:13 -0500

    starting projecdt added

---
+++ start/chapter10/OutputCacheRedis/BooksAPI/Repositories/IBooksRepository.cs
@@ -0,0 +1,12 @@
+using Books.Models;
+namespace Books.Repositories;
+
+public interface IBooksRepository
+{
+    Task<IReadOnlyCollection<Book>> GetBooksAsync(int pageSize, int lastId);
+    Task<Book?> GetBookByIdAsync(int id);
+    Task<Book> CreateBookAsync(Book book);
+    Task<Book?> GetBookByISBNAsync(string isbn);
+
+    IQueryable<Book> GetBooksAsQueryable();
+}
