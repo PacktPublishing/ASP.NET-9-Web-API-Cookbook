5f2d2b8cc1a406a694892c36343f44a818d00cd4
lavedon <lavedon@gmail.com>
2024-12-15 18:47:36 -0500

    problem details recipe setup complete.

--- end/chapter01/problemDetails/Program.cs
+++ end/chapter01/problemDetails/Program.cs
@@ -1,14 +1,18 @@
 using cookbook.Data;
 using cookbook.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using Bogus;
 using cookbook.Services;
-using System.Diagnostics;
+using Microsoft.Data.Sqlite;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+var connection = new SqliteConnection("DataSource=:memory:");
+connection.Open();
 
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
 builder.Services.AddProblemDetails(options =>
     options.CustomizeProblemDetails = (context) =>
     {
@@ -31,48 +35,40 @@ builder.Services.AddProblemDetails(options =>
             context.ProblemDetails.Title = "An unexpected error occurred";
             context.ProblemDetails.Detail = "An unexpected error occurred. Please try again later.";
         }
-});
+    });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
-
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlite(connection));
 builder.Services.AddScoped<IProductsService, ProductReadService>();
 
 var app = builder.Build();
-    
-    
 
 using (var scope = app.Services.CreateScope())
 {
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<AppDbContext>();
-
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     context.Database.EnsureCreated();
-
+    
     if (!context.Products.Any())
     {
         var productFaker = new Faker<Product>()
             .RuleFor(p => p.Name, f => f.Commerce.ProductName())
             .RuleFor(p => p.Price, f => f.Finance.Amount(50, 2000))
             .RuleFor(p => p.CategoryId, f => f.Random.Int(1, 5));
-
         var products = productFaker.Generate(10000);
-
         context.Products.AddRange(products);
         context.SaveChanges();
     }
 }
 
+app.MapControllers();
+app.MapOpenApi();
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
