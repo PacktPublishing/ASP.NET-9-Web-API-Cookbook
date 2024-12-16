9cdd608eaafb99abbbef5fe5ecb7cef4dd79b47e
lavedon <lavedon@gmail.com>
2024-12-15 21:22:26 -0500

    updated start and end of two chapters.

--- start/chapter01/keyset/Program.cs
+++ start/chapter01/keyset/Program.cs
@@ -1,54 +1,55 @@
 using cookbook.Data;
 using cookbook.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
 using Bogus;
 using cookbook.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Create and open a connection that will persist for the application lifetime
+var connection = new SqliteConnection("DataSource=:memory:");
+connection.Open();
 
+// Add services
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
-
+// Register DbContext with the shared connection
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlite(connection));
+    
 builder.Services.AddScoped<IProductsService, ProductReadService>();
 
 var app = builder.Build();
 
+// Initialize database
 using (var scope = app.Services.CreateScope())
 {
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<AppDbContext>();
-
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     context.Database.EnsureCreated();
-
+    
     if (!context.Products.Any())
     {
         var productFaker = new Faker<Product>()
             .RuleFor(p => p.Name, f => f.Commerce.ProductName())
             .RuleFor(p => p.Price, f => f.Finance.Amount(50, 2000))
             .RuleFor(p => p.CategoryId, f => f.Random.Int(1, 5));
-
         var products = productFaker.Generate(10000);
-
         context.Products.AddRange(products);
         context.SaveChanges();
     }
 }
 
-// Configure the HTTP request pipeline.
+app.MapControllers();
+app.MapOpenApi();
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
