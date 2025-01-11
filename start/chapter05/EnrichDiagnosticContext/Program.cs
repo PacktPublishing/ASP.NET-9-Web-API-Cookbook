b0ab18668c73f41b0e546f2dbfb0c6b7a925f6fc
lavedon <lavedon@gmail.com>
2025-01-11 16:27:16 -0500

    EnrichDiagnosticContext starter recipe

--- start/chapter05/EnrichDiagnosticContext/Program.cs
+++ start/chapter05/EnrichDiagnosticContext/Program.cs
@@ -1,42 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using Books.Data;
+using Books.Services;
+using Books.Repositories;
+using Scalar.AspNetCore;
 using Serilog;
-using Serilog.Events;
 
-namespace books;
+try {
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Information("Starting Web API");
+
+builder.ConfigureLogging();
+
+builder.Services.AddCors(options =>
 {
-    public static void Main(string[] args)
+    options.AddDefaultPolicy(builder =>
     {
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-            .Enrich.FromLogContext()
-            .WriteTo.Console()
-            .WriteTo.Seq("http://localhost:5341")
-            .CreateLogger();
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
 
-        try
-        {
-            
-            Log.Information("Starting Web API");
-            CreateHostBuilder(args).Build().Run();
-        }
-        catch (Exception ex)
-        {
-            Log.Fatal(ex, "Application start-up failed");
-        }
-        finally
-        {
-            Log.CloseAndFlush();
-        }
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi("chapter5");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IBooksRepository, BooksRepository>();
+builder.Services.AddScoped<IBooksService, BooksService>();
 
-    }
+var app = builder.Build();
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-        .UseSerilog()
-        .ConfigureWebHostDefaults(webBuilder =>
-        {
-                webBuilder.UseStartup<Startup>();
-        });
+app.UseSerilogRequestLogging();
+app.UseResponseCaching();
+app.MapOpenApi();
+app.UseCors();
+app.UseRouting();
+app.MapControllers();
+app.MapScalarApiReference();
+
+using (var scope = app.Services.CreateScope()) 
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    DatabaseSeeder.Initialize(context);
+}
+
+app.Run();
+} 
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application start-up failed");
+    throw;
+}
+finally 
+{
+    Log.CloseAndFlush();
 }
