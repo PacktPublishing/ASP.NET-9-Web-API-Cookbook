74ac71cf990631a92cef6ded0906bdb3ceaa64e6
lavedon <lavedon@gmail.com>
2025-01-10 21:34:14 -0500

    removed old end project for seqeveryrequest

--- end/chapter05/SeqEveryRequest/Repositories/IBooksRepository.cs
+++
@@ -1,9 +0,0 @@
-using books.Models;
-namespace books.Repositories;
-
-public interface IBooksRepository
-{
-    Task<IReadOnlyCollection<Book>> GetBooksAsync(int pageSize, int lastId);
-    Task<Book?> GetBookByIdAsync(int id);
-    Task<Book> CreateBookAsync(Book book);
-}
