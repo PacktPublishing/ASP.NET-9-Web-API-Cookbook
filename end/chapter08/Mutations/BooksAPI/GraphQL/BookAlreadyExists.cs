51a33a9b4d653997bb79b5aaa8d5daec0d0d1957
lavedon <lavedon@gmail.com>
2025-02-01 23:14:14 -0500

    chapter8 Mutations end project cleaned up.

--- end/chapter08/Mutations/BooksAPI/GraphQL/BookAlreadyExists.cs
+++ end/chapter08/Mutations/BooksAPI/GraphQL/BookAlreadyExists.cs
@@ -1,13 +1,11 @@
-namespace books.GraphQL
-{
-    public class BookAlreadyExistsError : IUserError
-    {
-        public string Message { get; }
-        public string Code => "BOOK_ALREADY_EXISTS";
+namespace Books.GraphQL;
 
-        public BookAlreadyExistsError(string isbn)
-        {
-            Message = $"A book with ISBN '{isbn}' already exists.";
-        }
+public class BookAlreadyExistsError : IUserError
+{
+    public string Message { get; }
+    public string Code => "BOOK_ALREADY_EXISTS";
+    public BookAlreadyExistsError(string isbn)
+    {
+      Message = $"A book with ISBN '{isbn}' already exists.";
     }
 }
